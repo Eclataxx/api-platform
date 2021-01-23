2ce4f0fb338db97111e5ad3231c3d50488e04e05
Tom Délié <tomdelie78@gmail.com>
2021-01-23 22:47:24 +0100

    Add product filter by name

--- api/src/Entity/Product.php
+++ api/src/Entity/Product.php
@@ -8,6 +8,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+
 
 /**
  * @ApiResource(
@@ -28,6 +31,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *
  * )
  * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * @ApiFilter(SearchFilter::class, properties={"name": "ipartial"})
  *
  */
 class Product
